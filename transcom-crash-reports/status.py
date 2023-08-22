fb04a4427674a53d60565dab2aa9b08caf7dd6fa
Andrew T. Barker <abarker@gmail.com>
2023-08-21 19:02:15 -0700

    Document workflow, status check

---
+++ transcom-crash-reports/status.py
@@ -0,0 +1,53 @@
+"""
+Copyright (c) 2023 Andrew T. Barker
+
+This software is distributed under the MIT License, see the LICENSE file
+or https://mit-license.org/
+"""
+
+"""
+This documents the workflow and progress we are making.
+
+It should be a checklist or document or something, but I am a nerd
+so it is a python script.
+"""
+
+import glob
+import json
+import os
+
+
+def main():
+    for fn in sorted(glob.glob("*_crashes.pdf")):
+        datetag = fn[:8]
+        print(datetag)
+        print("  have .pdf of crash report:", fn)
+        rawtext_file = fn.replace(".pdf", ".txt")
+        if os.path.exists(rawtext_file):
+            print("  have raw text:", rawtext_file)
+        else:
+            print("  no raw text, try parse.py:textfile")
+            continue
+        cleantext_file = rawtext_file.replace("crashes", "clean")
+        if os.path.exists(cleantext_file):
+            print("  have cleaned up text:", cleantext_file)
+        else:
+            print("  no clean text, clean it up by hand")
+            continue
+        basicjson = cleantext_file.replace(".txt", ".json")
+        try:
+            with open(basicjson, "r") as fd:
+                jo = json.load(fd)
+            entries = len(jo)
+            # TODO: check that date and time entries are there?
+            print("  basic .json file:", basicjson)
+        except FileNotFoundError:
+            print("  no basic .json file, try parse.py:parsetext")
+        # TODO: human.json check
+        # TODO: check existence of coordinates, other tags
+
+
+if __name__ == "__main__":
+    main()
+
+        
