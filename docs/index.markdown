d4d05c7cecbaffc2b13f10f09f9ac13d4983c3f9
Andrew T. Barker <abarker@gmail.com>
2023-11-29 19:30:43 -0800

    Add (some) links to table

--- docs/index.markdown
+++ docs/index.markdown
@@ -10,6 +10,20 @@ This site is an attempt to do real-time tracking of car crashes in Hillsboro, Or
 The [Transportation Committee](https://www.hillsboro-oregon.gov/our-city/commissions-committees-boards) appointed by the City Council receives a monthly report on some crashes in the city. The data here is compiled from those reports.
 
 
+## Fatal crashes
+
+| date | intersection | type | report |
+| --- | --- | --- | --- |
+| 2022-07-03 | NE Tanasbourne Drive and NE Stucki Avenue |  |  |
+| 2022-10-15 | SE 10th Avenue and SE Clipper Lane | pedestrian |  |
+| 2022-10-17 | NE Cornelius Pass Road and NE Nicholas Court | bicycle |  |
+| 2022-10-31 | S 1st Ave and SW Oak Street | pedestrian |  |
+| 2023-02-13 | NE Cornell Road and NE Stucki Avenue | pedestrian |  |
+| 2023-06-14 | E Main St and NE 55th Ave | bicycle | [link](https://hillsboro-oregon.civicweb.net/document/60479/TC%20Crash%20Report%20Memo%207.25.23.pdf) |
+| 2023-09-23 | SE 50th Avenue and E Main Street |  | [link](https://hillsboro-oregon.civicweb.net/document/164356/TC%20Crash%20Report%20Memo%2010.24.23.pdf) |
+| 2023-10-13 | SE Cornelius Pass Road and SW Augusta Lane |  | [link](https://hillsboro-oregon.civicweb.net/document/164356/TC%20Crash%20Report%20Memo%2010.24.23.pdf) |
+
+
 ## This site's past life
 
 Previously, we attempted to track crashes throughout Washington County, not just in Hillsboro. That got to be too much to manage, but some old data is still available:
--- transcom-crash-reports/table.py
+++ transcom-crash-reports/table.py
@@ -9,20 +9,39 @@ import json
 import os
 import sys
 
+links = {
+    "20230523": "https://hillsboro-oregon.civicweb.net/document/58545/TC%20Crash%20Report%20Memo%205.23.23.pdf",
+    "20230725": "https://hillsboro-oregon.civicweb.net/document/60479/TC%20Crash%20Report%20Memo%207.25.23.pdf",
+    "20230926": "https://hillsboro-oregon.civicweb.net/document/90968/TC%20Crash%20Report%20Memo%209.26.23.pdf",
+    "20231024": "https://hillsboro-oregon.civicweb.net/document/164356/TC%20Crash%20Report%20Memo%2010.24.23.pdf",
+    "20231128": "https://hillsboro-oregon.civicweb.net/document/230977/TC%20Crash%20Report%20Memo%2011.28.23.pdf"
+}
 
 def make_table(db):
     """db is a list of dicts, probably loaded from one or more human .json
     files
 
     Returns a string containing a markdown table"""
-    out = "| date | intersection |\n"
-    out += "| --- | --- |\n"
+    out = "| date | intersection | type | report |\n"
+    out += "| --- | --- | --- | --- |\n"
     for crash in db:
         if "fatal" not in crash["severity"]:
             continue
-        out += "|"
-        out += str(crash["date"]) + "|"
-        out += str(crash["street0"]) + " and " + str(crash["street1"]) + "|\n"
+        out += "| "
+        out += crash["date"] + " | "
+        out += crash["street0"] + " and " + crash["street1"] + " | "
+        if "pedestrian" in crash["description"].lower():
+            out += "pedestrian"
+        elif ("bicycle" in crash["description"].lower() or
+              "cyclist" in crash["description"].lower()):
+            out += "bicycle"
+        out += " | "
+        reportdate = crash["source"][:8]
+        try:
+            out += "[link](" + links[reportdate] + ")"
+        except KeyError:
+            pass
+        out += " |\n"
     return out
 
 
