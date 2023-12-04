9a2ea1842bd8d33954abb2daf4e402b15b8c0ca4
Andrew T. Barker <abarker@gmail.com>
2023-12-03 18:32:09 -0800

    Complete set of links in table.py

--- transcom-crash-reports/table.py
+++ transcom-crash-reports/table.py
@@ -9,7 +9,17 @@ import json
 import os
 import sys
 
+# TODO: maybe put links in a separate .json file or something
+# TODO: automatically include the table in index.markdown
+
 links = {
+    "20220628": "https://hillsboro-oregon.civicweb.net/document/45643/TC%20Crash%20Report%20Memo.pdf",
+    "20220726": "https://hillsboro-oregon.civicweb.net/document/46501/TC%20Crash%20Report%20Memo%207.21.pdf",
+    "20220927": "https://hillsboro-oregon.civicweb.net/document/49677/TC%20Crash%20Report%20Memo%209.12.22.pdf",
+    "20221025": "https://hillsboro-oregon.civicweb.net/document/50837/TC%20Crash%20Report%20Memo%2010.15.22.pdf",
+    "20221122": "https://hillsboro-oregon.civicweb.net/document/51927/TC%20Crash%20Report%20Memo%2011.9.22.pdf",
+    "20230124": "https://hillsboro-oregon.civicweb.net/document/54139/TC%20Crash%20Report%20Memo%201.19.23.pdf",
+    "20230425": "https://hillsboro-oregon.civicweb.net/document/57390/TC%20Crash%20Report%20Memo%204.25.23.pdf",
     "20230523": "https://hillsboro-oregon.civicweb.net/document/58545/TC%20Crash%20Report%20Memo%205.23.23.pdf",
     "20230725": "https://hillsboro-oregon.civicweb.net/document/60479/TC%20Crash%20Report%20Memo%207.25.23.pdf",
     "20230926": "https://hillsboro-oregon.civicweb.net/document/90968/TC%20Crash%20Report%20Memo%209.26.23.pdf",
@@ -22,7 +32,7 @@ def make_table(db):
     files
 
     Returns a string containing a markdown table"""
-    out = "| date | intersection | type | report |\n"
+    out = "| Date | Intersection | Type | Report |\n"
     out += "| --- | --- | --- | --- |\n"
     for crash in db:
         if "fatal" not in crash["severity"]:
