9eb03b59325c9510ae33bf1a46007185ddeee53a
Andrew T. Barker <abarker@gmail.com>
2023-12-08 19:14:56 -0800

    Better color for serious crashes, still not happy with it

--- transcom-crash-reports/map.py
+++ transcom-crash-reports/map.py
@@ -66,7 +66,8 @@ def crashes(ax, db):
             severity = ""
             victim = "vehicle"
             if "serious" in crash["severity"]:
-                color = (1.0, 1.0, 0.0)
+                # color = (1.0, 1.0, 0.0)
+                color = (0.6, 0.0, 0.8)
                 severity = "serious"
             if "fatal" in crash["severity"]:
                 color = (1.0, 0.0, 0.0)
