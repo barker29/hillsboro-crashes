da5112e5b20f18e2779b78a9113dadd7daa131f5
Andrew T. Barker <abarker@gmail.com>
2024-04-19 19:36:13 -0700

    Updates from 20240423 transcom report

--- transcom-crash-reports/table.py
+++ transcom-crash-reports/table.py
@@ -28,6 +28,7 @@ links = {
     "20240123": "https://hillsboro-oregon.civicweb.net/document/232854/TC%20Crash%20Report%20Memo%201.23.24.pdf",
     "20240227": "https://hillsboro-oregon.civicweb.net/document/233521/TC%20Crash%20Report%20Memo%202.9.24.pdf",
     "20240326": "https://hillsboro-oregon.civicweb.net/document/234638/TC%20Crash%20Report%20Memo%203.20.24.pdf",
+    "20240423": "https://hillsboro-oregon.civicweb.net/document/235693/TC%20Crash%20Report%20Memo%204.18.24.pdf",
 }
 
 def make_table(db):
